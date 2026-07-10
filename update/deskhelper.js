65ab5a581e8c3abfcfb56bc1dc007c92ed98bb07
Lisselde_E <Lisselde_E_Hamile@outlook.com>
2026-07-10 18:37:10 +0800

    R1.7

--- update/deskhelper.js
+++ update/deskhelper.js
@@ -8,23 +8,23 @@ window.__UPDATE_TREE__['deskhelper'] = {
       "tag": "最新",
       "changes": [
         {
-          "type": "improvement",
-          "text": "移动了配置文件保存路径",
-          "section": "#02"
-        },
-        {
           "type": "feature",
           "text": "新增PDF工具-转为word功能",
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复了哈希校验移入新文件老数据残留的问题",
-          "section": "#01"
+          "type": "improvement",
+          "text": "移动了配置文件保存路径",
+          "section": "#02"
         },
         {
           "type": "improvement",
           "text": "更新了配置文件保存路径",
+          "section": "#01"
+        },
+        {
+          "type": "fix",
+          "text": "修复了哈希校验移入新文件老数据残留的问题",
           "section": "#01"
         }
       ]
@@ -35,7 +35,12 @@ window.__UPDATE_TREE__['deskhelper'] = {
       "tag": "",
       "changes": [
         {
-          "type": "improvement",
+          "type": "note",
+          "text": "重要修复版本",
+          "section": ""
+        },
+        {
+          "type": "note",
           "text": "发布R5构建版本",
           "section": "#02"
         },
@@ -45,23 +50,8 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复了程序启动时偶现崩溃的问题",
-          "section": "#01"
-        },
-        {
-          "type": "fix",
-          "text": "修复了切换语言导致的程序崩溃问题",
-          "section": "#01"
-        },
-        {
-          "type": "fix",
-          "text": "修复了所有模块切换语言后部分控件文字显示异常的问题",
-          "section": "#01"
-        },
-        {
           "type": "feature",
-          "text": "新增批量重命名、批量创建",
+          "text": "新增功能-文件处理",
           "section": "#01"
         },
         {
@@ -71,13 +61,23 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "feature",
-          "text": "新增功能-文件处理",
+          "text": "新增批量重命名、批量创建",
           "section": "#01"
         },
         {
-          "type": "note",
-          "text": "重要修复版本",
-          "section": ""
+          "type": "fix",
+          "text": "修复了所有模块切换语言后部分控件文字显示异常的问题",
+          "section": "#01"
+        },
+        {
+          "type": "fix",
+          "text": "修复了切换语言导致的程序崩溃问题",
+          "section": "#01"
+        },
+        {
+          "type": "fix",
+          "text": "修复了程序启动时偶现崩溃的问题",
+          "section": "#01"
         }
       ]
     },
@@ -97,18 +97,13 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#04"
         },
         {
-          "type": "improvement",
-          "text": "优化程序内存堆栈，增加稳定性",
-          "section": "#03"
-        },
-        {
-          "type": "fix",
-          "text": "修复了重复名保存逻辑导致部分模块崩溃的问题",
+          "type": "feature",
+          "text": "新增功能-文件加密",
           "section": "#03"
         },
         {
           "type": "improvement",
-          "text": "优化了大文件加密/解密时的内存逻辑",
+          "text": "支持文件加密、加密文件解密",
           "section": "#03"
         },
         {
@@ -118,27 +113,22 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "improvement",
-          "text": "支持文件加密、加密文件解密",
+          "text": "优化了大文件加密/解密时的内存逻辑",
           "section": "#03"
         },
         {
-          "type": "feature",
-          "text": "新增功能-文件加密",
+          "type": "fix",
+          "text": "修复了重复名保存逻辑导致部分模块崩溃的问题",
           "section": "#03"
         },
         {
           "type": "improvement",
-          "text": "优化界面布局显示逻辑",
-          "section": "#02"
+          "text": "优化程序内存堆栈，增加稳定性",
+          "section": "#03"
         },
         {
           "type": "feature",
-          "text": "新增自定义二维码样式（颜色与背景色）",
-          "section": "#02"
-        },
-        {
-          "type": "feature",
-          "text": "新增二维码解码模式",
+          "text": "新增功能-二维码工具",
           "section": "#02"
         },
         {
@@ -148,22 +138,22 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "feature",
-          "text": "新增功能-二维码工具",
+          "text": "新增二维码解码模式",
           "section": "#02"
         },
         {
           "type": "feature",
-          "text": "添加了主界面的动态过渡动画",
-          "section": "#01"
-        },
-        {
-          "type": "feature",
-          "text": "新增-文件哈希校验功能",
-          "section": "#01"
+          "text": "新增自定义二维码样式（颜色与背景色）",
+          "section": "#02"
         },
         {
           "type": "improvement",
-          "text": "统一了各模块在顶部横幅通知只显示的信息样式",
+          "text": "优化界面布局显示逻辑",
+          "section": "#02"
+        },
+        {
+          "type": "fix",
+          "text": "修复了导出路径存在重复文件，会直接替换目标文件的问题",
           "section": "#01"
         },
         {
@@ -172,8 +162,18 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#01"
         },
         {
-          "type": "fix",
-          "text": "修复了导出路径存在重复文件，会直接替换目标文件的问题",
+          "type": "improvement",
+          "text": "统一了各模块在顶部横幅通知只显示的信息样式",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "新增-文件哈希校验功能",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "添加了主界面的动态过渡动画",
           "section": "#01"
         }
       ]
@@ -185,22 +185,17 @@ window.__UPDATE_TREE__['deskhelper'] = {
       "changes": [
         {
           "type": "feature",
+          "text": "新增默认用户桌面设置路径",
+          "section": "#03"
+        },
+        {
+          "type": "feature",
           "text": "新增国内下载源，智能切换优化下载更新体验",
           "section": "#03"
         },
         {
           "type": "feature",
-          "text": "新增默认用户桌面设置路径",
-          "section": "#03"
-        },
-        {
-          "type": "improvement",
-          "text": "证件照排版模式下，其他处理方式暂被禁用",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "排版可随意调整照片排布方式",
+          "text": "新增功能-图片处理-证件照排版",
           "section": "#02"
         },
         {
@@ -209,18 +204,18 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#02"
         },
         {
-          "type": "feature",
-          "text": "新增功能-图片处理-证件照排版",
+          "type": "improvement",
+          "text": "排版可随意调整照片排布方式",
           "section": "#02"
         },
         {
           "type": "improvement",
-          "text": "优化统一了所有模块的界面布局逻辑",
-          "section": "#01"
+          "text": "证件照排版模式下，其他处理方式暂被禁用",
+          "section": "#02"
         },
         {
-          "type": "improvement",
-          "text": "优化了所有模块进度条显示渲染逻辑",
+          "type": "feature",
+          "text": "新增功能-PDF处理",
           "section": "#01"
         },
         {
@@ -229,8 +224,13 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#01"
         },
         {
-          "type": "feature",
-          "text": "新增功能-PDF处理",
+          "type": "improvement",
+          "text": "优化了所有模块进度条显示渲染逻辑",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "优化统一了所有模块的界面布局逻辑",
           "section": "#01"
         }
       ]
@@ -241,13 +241,8 @@ window.__UPDATE_TREE__['deskhelper'] = {
       "tag": "",
       "changes": [
         {
-          "type": "improvement",
-          "text": "优化其他模块的输出路径显示",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "优化此界面通知横幅样式",
+          "type": "feature",
+          "text": "新增功能-格式转换",
           "section": "#02"
         },
         {
@@ -256,18 +251,18 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#02"
         },
         {
-          "type": "feature",
-          "text": "新增功能-格式转换",
+          "type": "improvement",
+          "text": "优化此界面通知横幅样式",
           "section": "#02"
         },
         {
           "type": "improvement",
-          "text": "优化程序启动速度",
-          "section": "#01"
+          "text": "优化其他模块的输出路径显示",
+          "section": "#02"
         },
         {
           "type": "feature",
-          "text": "新增启动时保存路径配置弹窗",
+          "text": "新增全局设置按钮",
           "section": "#01"
         },
         {
@@ -277,7 +272,12 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "feature",
-          "text": "新增全局设置按钮",
+          "text": "新增启动时保存路径配置弹窗",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "优化程序启动速度",
           "section": "#01"
         }
       ]
@@ -288,8 +288,8 @@ window.__UPDATE_TREE__['deskhelper'] = {
       "tag": "首发",
       "changes": [
         {
-          "type": "fix",
-          "text": "修复了调整窗口大小出现的按钮跳动问题",
+          "type": "feature",
+          "text": "新增功能-图片处理",
           "section": "#04"
         },
         {
@@ -298,13 +298,13 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#04"
         },
         {
-          "type": "feature",
-          "text": "新增功能-图片处理",
+          "type": "fix",
+          "text": "修复了调整窗口大小出现的按钮跳动问题",
           "section": "#04"
         },
         {
-          "type": "improvement",
-          "text": "完成了文件名提取的功能构建",
+          "type": "feature",
+          "text": "新增功能-文件名提取",
           "section": "#03"
         },
         {
@@ -313,34 +313,9 @@ window.__UPDATE_TREE__['deskhelper'] = {
           "section": "#03"
         },
         {
-          "type": "feature",
-          "text": "新增功能-文件名提取",
+          "type": "improvement",
+          "text": "完成了文件名提取的功能构建",
           "section": "#03"
-        },
-        {
-          "type": "improvement",
-          "text": "构建了程序的图标",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "完成了压缩部分的功能构建",
-          "section": "#02"
-        },
-        {
-          "type": "fix",
-          "text": "优化了按钮文字的显示逻辑，修复显示不全的问题",
-          "section": "#02"
-        },
-        {
-          "type": "feature",
-          "text": "添加按钮动画与过渡样式",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "优化信息通知样式",
-          "section": "#02"
         },
         {
           "type": "improvement",
@@ -349,7 +324,32 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "improvement",
-          "text": "适配多语言显示",
+          "text": "优化信息通知样式",
+          "section": "#02"
+        },
+        {
+          "type": "feature",
+          "text": "添加按钮动画与过渡样式",
+          "section": "#02"
+        },
+        {
+          "type": "fix",
+          "text": "优化了按钮文字的显示逻辑，修复显示不全的问题",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "完成了压缩部分的功能构建",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "构建了程序的图标",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "初步构建软件界面",
           "section": "#01"
         },
         {
@@ -359,7 +359,7 @@ window.__UPDATE_TREE__['deskhelper'] = {
         },
         {
           "type": "improvement",
-          "text": "初步构建软件界面",
+          "text": "适配多语言显示",
           "section": "#01"
         }
       ]
--- update/lansyncbox.js
+++ update/lansyncbox.js
@@ -8,23 +8,43 @@ window.__UPDATE_TREE__['lansyncbox'] = {
       "tag": "最新",
       "changes": [
         {
-          "type": "fix",
-          "text": "修复列表内复制操作导致的界面卡死问题",
+          "type": "note",
+          "text": "发布版本v6.6.0.0",
+          "section": "#06"
+        },
+        {
+          "type": "improvement",
+          "text": "优化加入房间操作体验",
+          "section": "#06"
+        },
+        {
+          "type": "improvement",
+          "text": "更新关于界面部分内容",
+          "section": "#06"
+        },
+        {
+          "type": "feature",
+          "text": "新增创建房间时可用性检查",
+          "section": "#06"
+        },
+        {
+          "type": "improvement",
+          "text": "优化可用性检查逻辑",
+          "section": "#06"
+        },
+        {
+          "type": "note",
+          "text": "发布版本v6.5.0.0",
           "section": "#05"
         },
         {
           "type": "feature",
-          "text": "修复添加大文件取消存在残留的问题",
+          "text": "新增保存快捷选项",
           "section": "#05"
         },
         {
           "type": "feature",
-          "text": "新增自定义房间号",
-          "section": "#05"
-        },
-        {
-          "type": "feature",
-          "text": "新增加入自动扫描已有房间",
+          "text": "新增列表文件框选",
           "section": "#05"
         },
         {
@@ -34,23 +54,23 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "feature",
-          "text": "新增列表文件框选",
+          "text": "新增加入自动扫描已有房间",
           "section": "#05"
         },
         {
           "type": "feature",
-          "text": "新增保存快捷选项",
-          "section": "#05"
-        },
-        {
-          "type": "note",
-          "text": "发布版本v6.5.0.0",
+          "text": "新增自定义房间号",
           "section": "#05"
         },
         {
           "type": "fix",
-          "text": "修复了偶现使用原虚拟路径的问题",
-          "section": "#04.1"
+          "text": "修复添加大文件取消存在残留的问题",
+          "section": "#05"
+        },
+        {
+          "type": "fix",
+          "text": "修复列表内复制操作导致的界面卡死问题",
+          "section": "#05"
         },
         {
           "type": "note",
@@ -58,14 +78,9 @@ window.__UPDATE_TREE__['lansyncbox'] = {
           "section": "#04.1"
         },
         {
-          "type": "improvement",
-          "text": "优化界面显示效果",
-          "section": "#04"
-        },
-        {
-          "type": "improvement",
-          "text": "优化文件预览时的处理逻辑",
-          "section": "#04"
+          "type": "fix",
+          "text": "修复了偶现使用原虚拟路径的问题",
+          "section": "#04.1"
         },
         {
           "type": "fix",
@@ -74,13 +89,13 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "improvement",
-          "text": "优化统一了部分逻辑内容",
-          "section": "#03"
+          "text": "优化文件预览时的处理逻辑",
+          "section": "#04"
         },
         {
           "type": "improvement",
-          "text": "优化了文件创建读取逻辑",
-          "section": "#03"
+          "text": "优化界面显示效果",
+          "section": "#04"
         },
         {
           "type": "fix",
@@ -89,17 +104,22 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "improvement",
+          "text": "优化了文件创建读取逻辑",
+          "section": "#03"
+        },
+        {
+          "type": "improvement",
+          "text": "优化统一了部分逻辑内容",
+          "section": "#03"
+        },
+        {
+          "type": "improvement",
           "text": "适配MSIX环境，将缓存路径迁移至AppData",
           "section": "#02.1"
         },
         {
-          "type": "feature",
-          "text": "同步界面移除同步状态，新增IP显示",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "更新支持新版本号信息对比验证",
+          "type": "fix",
+          "text": "修复了关于界面窗口关闭按钮无法点击的问题",
           "section": "#02"
         },
         {
@@ -108,18 +128,28 @@ window.__UPDATE_TREE__['lansyncbox'] = {
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复了关于界面窗口关闭按钮无法点击的问题",
+          "type": "improvement",
+          "text": "更新支持新版本号信息对比验证",
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复只读文件导致无法删除的问题",
+          "type": "improvement",
+          "text": "同步界面移除同步状态，新增IP显示",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "适配微软商店版本号显示",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "优化加入房间操作体验，验证失败直接在加入界面提示",
           "section": "#01"
         },
         {
           "type": "fix",
-          "text": "修复右键菜单条目逻辑异常的问题",
+          "text": "修复同步界面误触可直接退出的问题",
           "section": "#01"
         },
         {
@@ -129,17 +159,12 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "fix",
-          "text": "修复同步界面误触可直接退出的问题",
+          "text": "修复右键菜单条目逻辑异常的问题",
           "section": "#01"
         },
         {
-          "type": "improvement",
-          "text": "优化加入房间操作体验，验证失败直接在加入界面提示",
-          "section": "#01"
-        },
-        {
-          "type": "improvement",
-          "text": "适配微软商店版本号显示",
+          "type": "fix",
+          "text": "修复只读文件导致无法删除的问题",
           "section": "#01"
         }
       ]
@@ -155,28 +180,13 @@ window.__UPDATE_TREE__['lansyncbox'] = {
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复了新开重复端口房间连接端无法加入的错误",
-          "section": "#01"
-        },
-        {
-          "type": "fix",
-          "text": "修复了端口占用依旧可以新开房间的错误",
-          "section": "#01"
-        },
-        {
           "type": "improvement",
-          "text": "文件预览设置为只读",
+          "text": "修改程序缓存文件夹位置",
           "section": "#01"
         },
         {
           "type": "feature",
-          "text": "新增双击打开预览文件",
-          "section": "#01"
-        },
-        {
-          "type": "feature",
-          "text": "新增管理缓存功能",
+          "text": "新增加入房间双端版本号验证",
           "section": "#01"
         },
         {
@@ -186,12 +196,27 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "feature",
-          "text": "新增加入房间双端版本号验证",
+          "text": "新增管理缓存功能",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "新增双击打开预览文件",
           "section": "#01"
         },
         {
           "type": "improvement",
-          "text": "修改程序缓存文件夹位置",
+          "text": "文件预览设置为只读",
+          "section": "#01"
+        },
+        {
+          "type": "fix",
+          "text": "修复了端口占用依旧可以新开房间的错误",
+          "section": "#01"
+        },
+        {
+          "type": "fix",
+          "text": "修复了新开重复端口房间连接端无法加入的错误",
           "section": "#01"
         }
       ]
@@ -203,27 +228,27 @@ window.__UPDATE_TREE__['lansyncbox'] = {
       "changes": [
         {
           "type": "fix",
-          "text": "修复同步取消时导致的界面句柄溢出错误",
-          "section": "#02"
-        },
-        {
-          "type": "fix",
           "text": "修复同步取消时进度条残留的问题",
           "section": "#02"
         },
         {
           "type": "fix",
-          "text": "修复TCP缓冲区溢出的问题",
+          "text": "修复同步取消时导致的界面句柄溢出错误",
+          "section": "#02"
+        },
+        {
+          "type": "feature",
+          "text": "新增初次连接全量同步机制，对齐双端差异",
           "section": "#01"
         },
         {
-          "type": "improvement",
-          "text": "优化了程序在同步时的异步处理逻辑",
+          "type": "feature",
+          "text": "新增传输取消机制，处理传输中途文件变化导致传输损坏的问题",
           "section": "#01"
         },
         {
           "type": "fix",
-          "text": "修复了大量句柄溢出、残留问题",
+          "text": "完善传输队列，修复队列冲突导致文件跳过不同步的错误",
           "section": "#01"
         },
         {
@@ -233,17 +258,17 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "fix",
-          "text": "完善传输队列，修复队列冲突导致文件跳过不同步的错误",
+          "text": "修复了大量句柄溢出、残留问题",
           "section": "#01"
         },
         {
-          "type": "feature",
-          "text": "新增传输取消机制，处理传输中途文件变化导致传输损坏的问题",
+          "type": "improvement",
+          "text": "优化了程序在同步时的异步处理逻辑",
           "section": "#01"
         },
         {
-          "type": "feature",
-          "text": "新增初次连接全量同步机制，对齐双端差异",
+          "type": "fix",
+          "text": "修复TCP缓冲区溢出的问题",
           "section": "#01"
         }
       ]
@@ -254,23 +279,8 @@ window.__UPDATE_TREE__['lansyncbox'] = {
       "tag": "",
       "changes": [
         {
-          "type": "improvement",
-          "text": "优化了部分文字显示内容",
-          "section": "#02"
-        },
-        {
-          "type": "fix",
-          "text": "修复了部分已知错误",
-          "section": "#02"
-        },
-        {
-          "type": "fix",
-          "text": "修复大文件同步导致的错误",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "优化多连接情况下的同步逻辑",
+          "type": "feature",
+          "text": "添加，优化了所有同步逻辑",
           "section": "#02"
         },
         {
@@ -279,33 +289,28 @@ window.__UPDATE_TREE__['lansyncbox'] = {
           "section": "#02"
         },
         {
-          "type": "feature",
-          "text": "添加，优化了所有同步逻辑",
+          "type": "improvement",
+          "text": "优化多连接情况下的同步逻辑",
+          "section": "#02"
+        },
+        {
+          "type": "fix",
+          "text": "修复大文件同步导致的错误",
+          "section": "#02"
+        },
+        {
+          "type": "fix",
+          "text": "修复了部分已知错误",
           "section": "#02"
         },
         {
           "type": "improvement",
-          "text": "暂未实现同步逻辑",
-          "section": "#01"
+          "text": "优化了部分文字显示内容",
+          "section": "#02"
         },
         {
           "type": "improvement",
-          "text": "初步构建了界面UI逻辑",
-          "section": "#01"
-        },
-        {
-          "type": "improvement",
-          "text": "优化界面元素显示逻辑",
-          "section": "#01"
-        },
-        {
-          "type": "improvement",
-          "text": "优化大量界面交互逻辑，提升使用体验",
-          "section": "#01"
-        },
-        {
-          "type": "improvement",
-          "text": "使用全新QT6架构",
+          "text": "完全弃置之前版本，重新构建此程序",
           "section": "#01"
         },
         {
@@ -315,7 +320,27 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "improvement",
-          "text": "完全弃置之前版本，重新构建此程序",
+          "text": "使用全新QT6架构",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "优化大量界面交互逻辑，提升使用体验",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "优化界面元素显示逻辑",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "初步构建了界面UI逻辑",
+          "section": "#01"
+        },
+        {
+          "type": "improvement",
+          "text": "暂未实现同步逻辑",
           "section": "#01"
         }
       ]
@@ -326,13 +351,8 @@ window.__UPDATE_TREE__['lansyncbox'] = {
       "tag": "",
       "changes": [
         {
-          "type": "feature",
-          "text": "新增对于大文件同步的支持",
-          "section": "#02"
-        },
-        {
-          "type": "improvement",
-          "text": "移除了允许连接端互相同步，现在所有端都保持一致",
+          "type": "fix",
+          "text": "重构代码逻辑，修复了部分已知错误",
           "section": "#02"
         },
         {
@@ -341,18 +361,23 @@ window.__UPDATE_TREE__['lansyncbox'] = {
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "重构代码逻辑，修复了部分已知错误",
+          "type": "improvement",
+          "text": "移除了允许连接端互相同步，现在所有端都保持一致",
           "section": "#02"
         },
         {
-          "type": "fix",
-          "text": "修复了文件修改时异常删除信号的错误",
+          "type": "feature",
+          "text": "新增对于大文件同步的支持",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "实现多语言切换，适配多语言环境",
           "section": "#01"
         },
         {
           "type": "improvement",
-          "text": "优化程序启动速度，优化程序使用体验",
+          "text": "优化程序检查更新逻辑",
           "section": "#01"
         },
         {
@@ -362,12 +387,12 @@ window.__UPDATE_TREE__['lansyncbox'] = {
         },
         {
           "type": "improvement",
-          "text": "优化程序检查更新逻辑",
+          "text": "优化程序启动速度，优化程序使用体验",
           "section": "#01"
         },
         {
-          "type": "improvement",
-          "text": "实现多语言切换，适配多语言环境",
+          "type": "fix",
+          "text": "修复了文件修改时异常删除信号的错误",
           "section": "#01"
         }
       ]
