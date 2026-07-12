a2b448393b576642c4cae6bf1708f4ef5d071d7f
Lisselde_E <Lisselde_E_Hamile@outlook.com>
2026-07-12 16:22:43 +0800

    R2.1

---
+++ update/tokenpeek.js
@@ -0,0 +1,53 @@
+window.__UPDATE_TREE__ = window.__UPDATE_TREE__ || {};
+window.__UPDATE_TREE__['tokenpeek'] = {
+  "project": "tokenpeek",
+  "versions": [
+    {
+      "version": "R1",
+      "date": "2026.7.12",
+      "tag": "首发",
+      "changes": [
+        {
+          "type": "improvement",
+          "text": "修改了界面显示内容",
+          "section": "#02"
+        },
+        {
+          "type": "feature",
+          "text": "新增历史记录",
+          "section": "#02"
+        },
+        {
+          "type": "improvement",
+          "text": "优化界面布局",
+          "section": "#02"
+        },
+        {
+          "type": "feature",
+          "text": "新增多平台支持（智谱）",
+          "section": "#02"
+        },
+        {
+          "type": "feature",
+          "text": "初次构建程序",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "实现了DeepSeek API的调用",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "实现了界面基础显示效果",
+          "section": "#01"
+        },
+        {
+          "type": "feature",
+          "text": "实现了基本的用户交互功能",
+          "section": "#01"
+        }
+      ]
+    }
+  ]
+};
